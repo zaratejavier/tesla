6f1f72f0c75e7dd554c50b111fd3679ffc6db1fa
Javier Zarate <zarate.javier.jz@gmail.com>
2022-01-19 20:10:37 -0700

    tesla front page done

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+ 
+function Home() {
+ return (
+   <Container>
+     <Section
+       title="Model 3"
+       description="Order Online for Touchless delivery"
+       backgroundImg="model-3.jpg"
+       leftBtnText="Custom order"
+       rightBtnText="Existing inventory"
+     />
+     <Section
+       title="Model Y"
+       description="Order Online for Touchless delivery"
+       backgroundImg="model-y.jpg"
+       leftBtnText="Custom order"
+       rightBtnText="Existing inventory"
+     />
+     <Section
+       title="Model S"
+       description="Order Online for Touchless delivery"
+       backgroundImg="model-s.jpg"
+       leftBtnText="Custom order"
+       rightBtnText="Existing inventory"
+     />
+     <Section
+       title="Model X"
+       description="Order Online for Touchless delivery"
+       backgroundImg="model-x.jpg"
+       leftBtnText="Custom order"
+       rightBtnText="Existing inventory"
+     />
+     <Section
+       title="Lowest Cost Solar Panels in America"
+       description="Money-back guarantee"
+       backgroundImg="solar-panel.jpg"
+       leftBtnText="Custom order"
+       rightBtnText="Learn more"
+     />
+     <Section
+       title="Solar for New Roofs"
+       description="Solar Roof Cost Less Than a New Roof Plus Solar Panles"
+       backgroundImg="solar-roof.jpg"
+       leftBtnText="Custom order"
+       rightBtnText="Learn more"
+     />
+     <Section
+       title="Accessories"
+       description=""
+       backgroundImg="accessories.jpg"
+       leftBtnText="Shop Now"
+     />
+   </Container>
+ )
+}
+ 
+export default Home
+ 
+const Container = styled.div`
+ height: 100vh;
+`
